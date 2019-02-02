01a2f95d7321f2243fc8a6ec864420bce75e0431
Marcin Kunysz <marcink@dna-worldwide.co.uk>
2019-02-02 13:32:37 +0100

    user table change

--- routes.js
+++ routes.js
@@ -3,25 +3,21 @@
 class Routes {
   constructor(app, io){
     this.io = io;
-    this.users = [];
+    this.users = {};
   }
 
   socketEvents(io) {
     io.on('connection', (socket) => {
       socket.on('connected', (user) => {
-        user.socketId = socket.id;
-        this.users.push(user);
-        io.emit('connected', user);
+        this.users[socket.id] = user;
+        io.emit('users', this.users);
       });
 
-      socket.on('disconnected', (user) => {
-        for(let i = 0; i < this.users.length; i++) {
-          if (user.name === this.users[i].name){
-            this.users.splice(i, 1);
-            return;
-          }
-        }
+      socket.on('disconnect', () => {
+        delete this.users[socket.id];
+        io.emit('users', this.users);
       });
+
       socket.on('message', (message) => {
         io.emit('message', message);
       });
